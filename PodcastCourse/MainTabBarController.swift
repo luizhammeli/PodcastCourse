7fab87a20a96b93949b5b5b188cf993db7992ad5
Luiz Hammerli <luiz@whitebook.com.br>
2018-03-06 13:01:13 -0300

    first commit

---
+++ PodcastCourse/MainTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  MainTabBarController.swift
+//  PodcastCourse
+//
+//  Created by iOS Developer on 06/03/2018.
+//  Copyright © 2018 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = .purple
+        UINavigationBar.appearance().prefersLargeTitles = true
+        setUpViewControllers()
+    }
+    
+    //MARK:- Setup Function
+    func setUpViewControllers(){
+        self.viewControllers = [setUpTabBarItens(rootViewController: ViewController() , title: "Favorites", image: #imageLiteral(resourceName: "favorites")), setUpTabBarItens(rootViewController: ViewController() , title: "Search", image: #imageLiteral(resourceName: "search")), setUpTabBarItens(rootViewController: ViewController() , title: "Downloads", image:#imageLiteral(resourceName: "downloads"))]
+    }
+    
+    fileprivate func setUpTabBarItens(rootViewController: UIViewController, title: String, image: UIImage)->UIViewController{
+        let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }		
+}
